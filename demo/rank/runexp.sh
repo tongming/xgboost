2d93568739f3f00a29539468b26c1b9da732b2d0
kalenhaha <chenkl198812@gmail.com>
2014-05-19 17:26:17 +0800

    small change

--- demo/rank/runexp.sh
+++ demo/rank/runexp.sh
@@ -1,11 +1,11 @@
-python trans_data.py train.txt mq2007-list.train mq2007-list.train.group
+python trans_data.py train.txt train train.group
 
-python trans_data.py test.txt mq2007-list.test mq2007-list.test.group
+python trans_data.py test.txt test test.group
 
-python trans_data.py vali.txt mq2007-list.vali mq2007-list.vali.group
+python trans_data.py vali.txt vali vali.group
 
-../../xgboost mq2007-list.conf
+../../xgboost rank.conf
 
-../../xgboost mq2007-list.conf task=pred model_in=0004.model
+../../xgboost rank.conf task=pred model_in=0004.model
 
 
--- demo/rank/wgetdata.sh
+++ demo/rank/wgetdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget http://research.microsoft.com/en-us/um/beijing/projects/letor/LETOR4.0/Data/MQ2007-list.rar
-unrar x MQ2007-list.rar
-mv -f MQ2007-list/Fold1/*.txt .
+wget http://research.microsoft.com/en-us/um/beijing/projects/letor/LETOR4.0/Data/MQ2008.rar
+unrar x MQ2008.rar
+mv -f MQ2008/Fold1/*.txt .
