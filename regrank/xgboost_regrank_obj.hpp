11ff22ffa71cafcf8bb53e3da2d4b365165c8c10
tqchen <tianqi.tchen@gmail.com>
2014-05-18 21:34:38 -0700

    ok

--- regrank/xgboost_regrank_obj.hpp
+++ regrank/xgboost_regrank_obj.hpp
@@ -354,10 +354,12 @@ namespace xgboost{
                     for (size_t i = 0; i < pairs.size(); ++i){                    
                         unsigned pos_idx = pairs[i].pos_index;
                         unsigned neg_idx = pairs[i].neg_index;
+                        float pos_loginv = 1.0f / logf(pos_idx+2.0f);
+                        float neg_loginv = 1.0f / logf(neg_idx+2.0f);
                         int pos_label = static_cast<int>(sorted_list[pos_idx].label);
                         int neg_label = static_cast<int>(sorted_list[neg_idx].label);
-                        float original = ((1<<pos_label)-1) / logf(pos_idx+2) + ((1<<neg_label)-1) / logf(neg_idx+2);
-                        float changed  = ((1<<neg_label)-1) / logf(pos_idx+2) + ((1<<pos_label)-1) / logf(neg_idx+2);
+                        float original = ((1<<pos_label)-1) * pos_loginv + ((1<<neg_label)-1) / neg_loginv;
+                        float changed  = ((1<<neg_label)-1) * pos_loginv + ((1<<pos_label)-1) / neg_loginv;
                         float delta = (original-changed) * IDCG;
                         if( delta < 0.0f ) delta = - delta;
                         pairs[i].weight = delta;
