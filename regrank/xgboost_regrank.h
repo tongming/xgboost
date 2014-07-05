1b87236927d3b3331a4c7d469356a70d929736d0
tqchen <tianqi.tchen@gmail.com>
2014-07-04 22:16:52 -0700

    backward compatibility

--- regrank/xgboost_regrank.h
+++ regrank/xgboost_regrank.h
@@ -135,11 +135,12 @@ namespace xgboost{
                 base_gbm.LoadModel(fi);
                 utils::Assert(fi.Read(&mparam, sizeof(ModelParam)) != 0);
                 // save name obj
-                size_t len;
-                utils::Assert( fi.Read(&len, sizeof(len)) != 0 );
-                name_obj_.resize( len );
-                if( len != 0 ){
-                  utils::Assert( fi.Read(&name_obj_[0], len*sizeof(char)) != 0 );
+                size_t len;                
+                if( fi.Read(&len, sizeof(len)) != 0 ){
+                    name_obj_.resize( len );
+                    if( len != 0 ){
+                        utils::Assert( fi.Read(&name_obj_[0], len*sizeof(char)) != 0 );
+                    }
                 }
             }
             /*!
